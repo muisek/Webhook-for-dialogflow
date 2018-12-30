00d45e63ae3c703a801b49ffb73506612c1630eb
muisek <46251997+muisek@users.noreply.github.com>
2018-12-29 20:01:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ restService.post("/storytime", function(req, res) {
   return res.json({
     speech: speech,
     displayText: speech,
-    source: "webhook-echo-sample"
+    source: "dialogflow-storytime-app"
   });
 });
 
